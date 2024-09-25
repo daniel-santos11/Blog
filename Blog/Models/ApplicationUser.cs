fa54981d589e848b3cf3ab7cfaeabd4c9f431497
[daniel santos] <[daniel.santos.t0127534@edu.atec.pt]>
2024-09-25 18:00:36 +0100

    Adicionar arquivos de projeto.

---
+++ Blog/Models/ApplicationUser.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Blog.Models
+{
+    public class ApplicationUser:IdentityUser
+    {
+        public string? FirstName{ get; set; }
+        public string? LastName { get; set; }
+
+        //relação
+        public List<Post> Posts { get; set; }
+    }
+}
